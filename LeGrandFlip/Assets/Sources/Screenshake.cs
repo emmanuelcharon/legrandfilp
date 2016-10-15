54a1665ec95bd0341f55688f29d0118eb6078c89
Damien Blanchard <baal.chouchen@gmail.com>
2016-10-15 11:31:41 +0200

    add plugins itween & custom collider

---
+++ LeGrandFlip/Assets/Sources/Screenshake.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screenshake : MonoBehaviour {
+
+	private Hashtable ht = new Hashtable();
+
+	// Use this for initialization
+	void Start () 
+	{
+		ht.Add ("time", 10f);
+		iTween.ShakePosition (gameObject, ht);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Shake ()
+	{
+		
+	}
+}
