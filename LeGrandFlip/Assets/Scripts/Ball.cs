2d9a95df80babe1c15e30758b587fae9b43434f5
emmanuelcharon <emmanuelcharon@hotmail.com>
2016-10-15 12:06:55 +0200

    colliders

--- LeGrandFlip/Assets/Scripts/Ball.cs
+++ LeGrandFlip/Assets/Scripts/Ball.cs
@@ -6,11 +6,18 @@ public class Ball : MonoBehaviour {
 	Rigidbody2D rb;
 	float maxSpeed = 3000f;
 
+	public TrailRenderer trailRenderer;
+	float minTrailSpeed = 2200f;
+
+
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
 	}
 	
 	void Update () {
 		rb.velocity = Vector2.ClampMagnitude (rb.velocity, maxSpeed);
+
+		trailRenderer.enabled = rb.velocity.magnitude > minTrailSpeed; 
+
 	}
 }
